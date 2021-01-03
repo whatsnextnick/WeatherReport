be9cfab8866a6432b196c5ce66a794bb4f90608c
Jawascript <n.jmonplaisir@gmail.com>
2021-01-03 17:36:01 -0500

    Adding express server/ openweather api get request

--- script.js
+++ script.js
@@ -9,14 +9,54 @@ $(document).ready(function() {
   });
 
   //Retrieve city value 
-  $("#submitbtn").on("click", function() {
+  $("#submitbtn").click(function() {
     event.preventDefault();
     
       
-      city = $("#search").val();
-      $("#search").val("");
-      console.log(city);
-      //add city to search history list 
-      $("#history").append("<li>").addClass("list-group-item").text(city);
+    city = $("#search").val();
+    console.log(city);
+    //add city to search history list and show max 10 li
+    if ($("#history li").length != 10) {
+      $("#history").prepend("<li class='collection-item'>"+ city +"</li>");
+    }
+    else {$("#history").prepend("<li>"+ city +"</li>");
+      $("#history li:last-child").remove();}
+
+    //express api call 
+    const express = require("express");
+
+    const https = require("https");
+
+    const app = express();
+
+    app.get("/", function(req, res){
+      var apiKey = "&appid=5d604db65fd998007c9f84ed1979695a";
+      var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+
+      https.get(url, function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data) {
+          const weatherData = JSON.parse(data)
+          const temp = weatherData.main.temp
+
+          console.log(temp)
+        })
+      })
+    })
+
+
+
+
+
+
+
+    app.listen(1000, function() {
+      console.log("Server is running on port 3000");
+    })
+
+
+    var apiKey= "5d604db65fd998007c9f84ed1979695a2"
+
   })  
 });
