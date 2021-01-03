33d6ae38c39b918a14e3b3281c877986dcac264d
Jawascript <n.jmonplaisir@gmail.com>
2021-01-03 00:33:19 -0500

    Added js functionality to the search section in script.js file

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// return current and future conditions for city and add it to search history
+$(document).ready(function() {
+  // search box/button functionality
+  $("#search").keypress(function(event) { 
+
+    if (event.keyCode === 13) { 
+      event.preventDefault();
+      $("#submitbtn").click(); 
+    } 
+  });
+
+  //Retrieve city value 
+  $("#submitbtn").on("click", function() {
+    event.preventDefault();
+    
+      
+      city = $("#search").val();
+      $("#search").val("");
+      console.log(city);
+      //add city to search history list 
+      $("#history").append("<li>").addClass("list-group-item").text(city);
+  })  
+});
