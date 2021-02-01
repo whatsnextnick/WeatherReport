f53f48797f0e8348adbc8b125800e68aaef42b79
Jawascript <n.jmonplaisir@gmail.com>
2021-02-01 10:54:51 -0500

    successful GET request

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 
     if (event.keyCode === 13) { 
       event.preventDefault();
-      $("#submitbtn").click(); 
-    } 
+      $("#submitbtn").click();
+    };
   });
 
   //Retrieve city value 
@@ -13,7 +13,7 @@ $(document).ready(function() {
     event.preventDefault();
     
       
-    city = $("#search").val();
+    var city = $("#search").val();
     console.log(city);
     //add city to search history list and show max 10 li
     if ($("#history li").length != 10) {
@@ -22,41 +22,35 @@ $(document).ready(function() {
     else {$("#history").prepend("<li>"+ city +"</li>");
       $("#history li:last-child").remove();}
 
-    //express api call 
-    const express = require("express");
+    $("#search").val("");
+    $("#cityname").append($("<h2>").addClass("card-title").text(city));
+    
+  var apiKey = "&appid=5d604db65fd998007c9f84ed1979695a";
+  var currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city  + apiKey;
+    
+  // Get and post data per city
+  $.ajax({
+    url: currentWeatherURL,
+    method: "GET"
+  })
+  .then(function (response){
+  console.log(response.name);
+    function displayweather (){
+      $("#cityname").append($("<h2>").addClass("card-title").text(response.name));
+      $("#f-icon").append(weatherIcon);
+      var cityName = $("<h2>").addClass("card-title").text(city);
+      var weatherIcon = $("img").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png"); 
+      //var currentDate = $("<h3>").addClass("card-title").text(currentDate.toLocaleDateString("en-US"));
+      var temp = $("<p>")
+    
+    };
+  displayweather();
+  });
+  //var forecastURL = "http://api.openweathermap.org/data/2.5/forecasat?q=" + city  + apiKey;
 
-    const https = require("https");
-
-    const app = express();
-
-    app.get("/", function(req, res){
-      var apiKey = "&appid=5d604db65fd998007c9f84ed1979695a";
-      var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
-
-      https.get(url, function(response){
-        console.log(response.statusCode);
-
-        response.on("data", function(data) {
-          const weatherData = JSON.parse(data)
-          const temp = weatherData.main.temp
-
-          console.log(temp)
-        })
-      })
-    })
-
-
-
-
-
-
-
-    app.listen(1000, function() {
-      console.log("Server is running on port 3000");
-    })
-
-
-    var apiKey= "5d604db65fd998007c9f84ed1979695a2"
-
-  })  
+  // $.ajax({
+    //url: forecastURL,
+    //method: "GET"
+  //})
+  }); 
 });
